59b74e9d47a1bf9f3f0ba979b69abd462047aacf
Volodymyr Stakhovskyi <volodymyr.stakhovsky@TX.local>
2016-09-28 20:56:35 +0300

    fix popups 2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,16 @@
+jQuery(function(){
+ 	$("#alerts").on('hidden.bs.modal', function (e) {
+ 		$("#settings").modal("show");
+ 	});
+ 	$("#show-settings").click(function(){
+ 		$("#alerts").modal("hide")
+ 	})
 
-$(".hover").mouseleave(
-    function () {
-      $(this).removeClass("hover");
-    }
-  );
+
+	$(".hover").mouseleave(
+		function () {
+		  $(this).removeClass("hover");
+		}
+	);
+ 	
+})
