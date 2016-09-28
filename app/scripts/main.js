9b889a6a4835c7885c1e03b1b6684053a2763959
ira-kabalyuk <jokky@bigmir.net>
2016-09-28 11:43:48 +0300

    animation img dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,6 @@
 
-jQuery(function(){
-	$("#alerts").on('hidden.bs.modal', function (e) {
-		$("#settings").modal("show");
-	});
-	$("#show-settings").click(function(){
-		$("#alerts").modal("hide")
-	})
-	
-})
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
