83d9f506a26b58e8473d394e0e60848fe7e6ead5
Vovas <v.stakhovskiy@gmail.com>
2016-09-30 22:00:25 +0300

    padding fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,5 +14,24 @@ jQuery(function(){
 		  $(this).removeClass("hover");
 		}
 	);
+
+	$('#alerts, #settings').on('show.bs.modal', function() {
+		var pad = measureScrollbar();
+		$('#menu_block').css({'width': '+=0'});
+	})
+
+	$('#alerts, #settings').on('hidden.bs.modal', function() {
+		$('#menu_block').css('width', '100%');
+	})
  	
 })
+
+function measureScrollbar() {
+    var $body = $(document.body)
+    var scrollDiv = document.createElement('div')
+    scrollDiv.className = 'modal-scrollbar-measure'
+    $body.append(scrollDiv)
+    var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth
+    $body[0].removeChild(scrollDiv)
+    return scrollbarWidth
+}
